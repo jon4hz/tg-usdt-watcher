5751c297f43a9dc8e3a9bab4cac24e75afa2e3bc
jon4h <admin@psytux.com>
2021-01-08 03:36:27 +0100

    remove unused var

--- watcher.py
+++ watcher.py
@@ -13,8 +13,6 @@ from coinmarketcapapi import CoinMarketCapAPI, CoinMarketCapAPIError
 config = configparser.ConfigParser()
 config.read('data/config.ini')
 
-TETHER_CONTRACT_ADDRESS = '0xdac17f958d2ee523a2206206994597c13d831ec7'
-
 
 def getSupply(cmc) -> int:
     
